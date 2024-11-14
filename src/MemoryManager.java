2441db614d28c94787cc41930523e81dec1037b3
nimayg9 <ngoradia@vt.edu>
2024-11-14 14:07:03 -0500

    finished writing the memory manager class

--- src/MemoryManager.java
+++ src/MemoryManager.java
@@ -36,47 +36,51 @@ public class MemoryManager {
      * @return the handle that is inserted
      */
     public Handle insert(byte[] serializedData, int size) {
-        Boolean isInserted = false;
-        for (int i = 0; i < freeBlockList.size(); i++) {
-            if (freeBlockList.get(i).getLength() >= size) {
-                if (freeBlockList.get(i).getLength() == size) {
-                    freeBlockList.remove(i);
+        for (int listIndex = 0; listIndex < freeBlockList.size(); listIndex++) {
+            Block currentFreeBlock = freeBlockList.get(listIndex);
+            if (currentFreeBlock.getLength() >= size) {
+                if (currentFreeBlock.getLength() == size) {
+                    freeBlockList.remove(listIndex);
                 }
                 else {
-                    freeBlockList.get(i).setPosition(i + size);
+                    currentFreeBlock.setPosition(listIndex + size);
+                    currentFreeBlock.setLength(currentFreeBlock.getLength()
+                        - size);
                 }
-                System.arraycopy(serializedData, 0, memoryPool, i, size);
-                isInserted = true;
+                int position = currentFreeBlock.getPosition();
+                System.arraycopy(serializedData, 0, memoryPool, position, size);
+                return new Handle(position, size);
             }
         }
-        if (!isInserted) {
-
-        }
+        // If no suitable block found, expand memory and retry insertion
+        expand();
+        return insert(serializedData, size);
     }
 
 
     /**
-     * Expands the memory size
+     * Expands the memory size if no spot found is big enough
      */
     private void expand() {
         byte[] largerMemoryPool = new byte[memoryPool.length + initialMemSize];
         System.arraycopy(memoryPool, 0, largerMemoryPool, 0, memoryPool.length);
-        memoryPool = largerMemoryPool;
 
-        // Create a new block for the newly added memory space
-        Block newBlock = new Block(memoryPool.length - initialMemSize,
-            initialMemSize);
+        // create a new block for the new memory pool space
+        Block newBlock = new Block(memoryPool.length, initialMemSize);
 
-        // Check if it can be merged with the last block in the free list
+        // check if it can be merged with the last block
         Block lastBlock = freeBlockList.get(freeBlockList.size() - 1);
-        if (lastBlock.getPosition() + lastBlock.getLength() == newBlock.getPosition()) {
-            // Merge the new block with the last block in freeBlockList
+        if (lastBlock.getPosition() + lastBlock.getLength() == newBlock
+            .getPosition()) {
+            // merge new block and last block if needed
             lastBlock.setLength(lastBlock.getLength() + newBlock.getLength());
         }
         else {
-            // Add the new block as a separate free block
+            // otherwise add as a new block to the end
             freeBlockList.add(newBlock);
         }
+        memoryPool = largerMemoryPool;
+
     }
 
 
@@ -87,8 +91,64 @@ public class MemoryManager {
      *            is the handle that is removed
      */
     public void remove(Handle handle) {
-        // TODO Auto-generated method stub
+        int startPosition = handle.getPosition();
 
+        // new block of free memory
+        Block freedBlock = new Block(startPosition, handle.getLength());
+
+        // put the freed block into the freeBlockList in the correct position
+        int insertIndex;
+        for (insertIndex = 0; insertIndex < freeBlockList
+            .size(); insertIndex++) {
+            if (freeBlockList.get(insertIndex).getPosition() > startPosition) {
+                break;
+            }
+        }
+        freeBlockList.add(insertIndex, freedBlock);
+
+        // Check for possible merges with neighboring blocks
+        mergePrevAndNextBlocks(insertIndex);
+    }
+
+
+    /**
+     * Merges the block at the specified index with adjacent blocks if possible.
+     *
+     * @param index
+     *            the index of the newly added block in freeBlockList
+     */
+    private void mergePrevAndNextBlocks(int index) {
+        Block currentBlock = freeBlockList.get(index);
+
+        // if not the first block
+        if (index > 0) {
+            // merge with the previous block if they are adjacent
+            Block prevBlock = freeBlockList.get(index - 1);
+            if (prevBlock.getPosition() + prevBlock.getLength() == currentBlock
+                .getPosition()) {
+                prevBlock.setLength(prevBlock.getLength() + currentBlock
+                    .getLength());
+                // remove the merged block
+                freeBlockList.remove(index);
+                // point to the merged block
+                index--;
+                // update currentBlock reference for next if statement
+                currentBlock = prevBlock;
+            }
+        }
+
+        // if not the last block
+        if (index < freeBlockList.size() - 1) {
+            // merge with the next block if they are adjacent
+            Block nextBlock = freeBlockList.get(index + 1);
+            if (currentBlock.getPosition() + currentBlock
+                .getLength() == nextBlock.getPosition()) {
+                currentBlock.setLength(currentBlock.getLength() + nextBlock
+                    .getLength());
+                // remove the merged block
+                freeBlockList.remove(index + 1);
+            }
+        }
     }
 
 
@@ -101,10 +161,12 @@ public class MemoryManager {
      *            The handle of that record
      * @param length
      *            The length of the record
+     * @return the number of copied bytes is returned
      */
-    public void get(byte[] space, Handle handle, int size) {
-        // TODO Auto-generated method stub
-
+    public int get(byte[] space, Handle handle, int size) {
+        System.arraycopy(memoryPool, handle.getPosition(), space, 0, size);
+        // TODO what to return, ask a TA
+        return size;
     }
 
 
@@ -112,8 +174,14 @@ public class MemoryManager {
      * Dumps a printout of the freeblock list
      */
     public void dump() {
-        // TODO Auto-generated method stub
-
+        System.out.println("Free Block List:");
+        for (int i = 0; i < freeBlockList.size(); i++) {
+            Block block = freeBlockList.get(i);
+            System.out.print("(" + block.getPosition() + "," + block.getLength()
+                + ")");
+            if (i != freeBlockList.size() - 1) {
+                System.out.print(" -> ");
+            }
+        }
     }
-
 }
